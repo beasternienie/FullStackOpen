3f10eadd64130be28fba234982aa0ef0cec6b40b
beasternienie <nieniedjones@gmail.com>
2025-08-01 15:15:12 -0500

    Display "no feedback" message when there are no statistics to return.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -31,17 +31,27 @@ const Stats = (props) =>{
     let positiveCount = good / all
     let positive = positiveCount * 100
 
-    return (
-        <div>
-            <h1>Statistics</h1>
-            <p>Good {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
-            <p>All {all}</p>
-            <p>Average {average}</p>
-            <p>Positive {positive} %</p>
-        </div>
-    )
+    if(all > 0) {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <p>Good {good}</p>
+                <p>Neutral {neutral}</p>
+                <p>Bad {bad}</p>
+                <p>All {all}</p>
+                <p>Average {average}</p>
+                <p>Positive {positive} %</p>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <p>No feedback given.</p>
+            </div>
+        )
+    }
 }
 
 const App = () => {
