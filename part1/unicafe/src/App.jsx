4f9161652b30ec74423c00098179e03a1991be5b
beasternienie <nieniedjones@gmail.com>
2025-08-01 14:50:50 -0500

    Moved button functionality to its own component. Added statistics panel to render statistics for each feedback rating.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,72 @@
 import { useState } from 'react'
 
+// Header
+const Header = (props) =>{
+    return (
+        <h1>{props.header}</h1>
+    )
+}
+
+// Buttons
+const Button = (props) =>{
+    return (
+        <button onClick = {props.onClick}>{props.name}</button>
+    )
+}
+
+// Statistics Panel
+const Stats = (props) =>{
+
+    // Assign the props values to variables.
+    const [good, neutral, bad] = props.states
+
+    // Calculate the total value of all stats.
+    let all = 0
+    props.states.forEach(state => {all += state})
+
+    // Calculate the average.
+    let average = ((good * 1) + (bad * -1)) / all
+
+    // Calculate the positive percentage.
+    let positiveCount = good / all
+    let positive = positiveCount * 100
+
+    return (
+        <div>
+            <h1>Statistics</h1>
+            <p>Good {good}</p>
+            <p>Neutral {neutral}</p>
+            <p>Bad {bad}</p>
+            <p>All {all}</p>
+            <p>Average {average}</p>
+            <p>Positive {positive} %</p>
+        </div>
+    )
+}
+
 const App = () => {
+
+    const header = 'Give Feedback'
+
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const states = [good, neutral, bad]
+
+    // Event methods. Add one to the relevant value.
+    const increaseGood = () => setGood(good + 1)
+    const increaseNeutral = () => setNeutral(neutral + 1)
+    const increaseBad = () => setBad(bad + 1)
+
     return (
         <div>
-            code here
+            <Header header={header} />
+            <Button name={'good'} onClick={increaseGood}/>
+            <Button name ={'neutral'} onClick={increaseNeutral} />
+            <Button name={'bad'} onClick={increaseBad}/>
+            <Stats states={states}></Stats>
         </div>
     )
 }
