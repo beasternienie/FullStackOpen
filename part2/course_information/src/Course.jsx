a595fbe08358d9f3a78d21fdaf32e1ce4de549e9
beasternienie <nieniedjones@gmail.com>
2025-08-04 16:32:54 -0500

    Added function to calculate the total number of exercises for the course.

--- part2/course_information/src/Course.jsx
+++ part2/course_information/src/Course.jsx
@@ -7,7 +7,7 @@ const Header = ({title}) =>{
 const Content = ({parts}) =>{
     return(
         <div>
-            {parts.map(part => <Part name={part.name} exercises={part.exercises} />)}
+            {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
         </div>
     )
 }
@@ -22,10 +22,19 @@ const Course = ({course}) =>{
     const title = course.name
     const parts = course.parts
 
+    // Calculate the sum of the exercises in the course.
+    const total = parts.reduce((acc, curr) =>{
+        // Get the exercises field value for the current part.
+        const exercises = curr.exercises
+        // Add the value to the accumulator and return.
+        return acc + exercises;
+    }, 0)
+
     return (
         <div>
             <Header title={title} />
             <Content parts={parts} />
+            <p>total of {total} exercises</p>
         </div>
     )
 }
